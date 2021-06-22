9f5283a5f985ffa955fdf2935ed67eae2f9819eb
Montauriol Tom <tom.montauriol@etu.univ-lyon1.fr>
2021-06-22 13:55:04 +0200

    Background removal for all (FF).

--- src/filter/background_removal.py
+++ src/filter/background_removal.py
@@ -5,14 +5,13 @@ import numpy as np
 class BackgroundRemoval:
     def __init__(self, background):
         self.background = background
-        self.grayBackground = cv2.cvtColor(background, cv2.COLOR_BGR2GRAY)
 
     def get_mask(self, image):
-        grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        sub = cv2.subtract(self.grayBackground, grayImage)
-        ret, thresh1 = cv2.threshold(sub, 10, 255, cv2.THRESH_BINARY)
-        cv2.erode(thresh1, np.ones((3,3), np.uint8), thresh1)
-        cv2.dilate(thresh1, np.ones((3,3), np.uint8), thresh1)
+        sub = cv2.subtract(self.background, image)
+        grayImage = cv2.cvtColor(sub, cv2.COLOR_BGR2GRAY)
+        ret, thresh1 = cv2.threshold(grayImage, 10, 255, cv2.THRESH_BINARY)
+        cv2.erode(thresh1, np.ones((3, 3), np.uint8), thresh1)
+        cv2.dilate(thresh1, np.ones((3, 3), np.uint8), thresh1)
         return thresh1
 
     def background_removal(self, image):
